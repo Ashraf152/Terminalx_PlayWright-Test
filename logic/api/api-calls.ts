92a5f524dfedbe70b29c131a4b9a31d9220b0e6d
Ashraf152 <ashraf.egbaria@gmail.com>
2024-01-12 00:06:51 +0200

    creat aoi call for ligin and auth setup for login

---
+++ logic/api/api-calls.ts
@@ -0,0 +1,15 @@
+import { patchRequest, postRequest } from "../../infra/api/apiRequest";
+import configjson from '../../configfiles/config.json'
+import { APIRequestContext } from "playwright";
+
+
+export class ApiCalls{
+
+    async performLogin(request:APIRequestContext,url: string, user:string,password:string){
+        const data ={
+            username: user,
+            password: password
+        }
+        return await postRequest(url,data,undefined,request)
+    }
+}
