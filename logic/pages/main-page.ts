591da89d768c8a4d35e327d3c49393d62ed5acb4
bakermhajna <49346558+bakermhajna@users.noreply.github.com>
2024-01-14 00:52:34 +0200

    logout test

--- logic/pages/main-page.ts
+++ logic/pages/main-page.ts
@@ -10,6 +10,8 @@ export class MainPage extends BasePage {
     private readonly addressManagementButton:Locator;
     private readonly wishlistbutton:Locator;
     private readonly personalInfoButton:Locator;
+    private readonly logoutButton:Locator
+    private readonly userbutton:Locator
 
     constructor(page: Page) {
         super(page);
@@ -17,6 +19,8 @@ export class MainPage extends BasePage {
         this.addressManagementButton=this.page.locator('//a[@href="/customer/address/?limit=10" and @class="tx-link-a links-item_3GWv tx-link_29YD underline_1zpu underline-hover_3GkV"]')
         this.wishlistbutton=this.page.locator('//a[@class="tx-link-a link_2L32 link-wishlist_1lmB tx-link_29YD"]')
         this.personalInfoButton=this.page.locator('//a[@href="/customer/account/edit" and @class="tx-link-a list-link_323s tx-link_29YD"]')
+        this.logoutButton=this.page.locator('//li[@class="list-item_2A1s logout_3ZNa"]//button')
+        this.userbutton=this.page.locator('//div[@class="profile_3CiC rtl_6mEj"]')
         this.initPage();
     }
 
@@ -38,6 +42,15 @@ export class MainPage extends BasePage {
     async clickOnPersonalInfoButton() {
         const state = await waitForElementToBeVisible(this.personalInfoButton,1000,5);
         await this.personalInfoButton.click();
+    }
+    async clickLogout(){
+        await waitForElementToBeVisible(this.logoutButton,1000,5);
+        await this.logoutButton.click()
+        await this.page.waitForLoadState("networkidle")
+    }
+    async getTextInLoginButton(){
+        const state = await waitForElementToBeVisible(this.userLoginButton,1000,5);
+        return (await this.userbutton.allInnerTexts())[0]
     }
     
 }
