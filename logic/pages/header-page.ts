49dd9334ca4193a5a33047e0f5af545bce25da1d
NoorNaif <noorab3@ac.sce.ac.il>
2024-01-17 14:52:24 +0200

    create Hover test

--- logic/pages/header-page.ts
+++ logic/pages/header-page.ts
@@ -1,16 +1,14 @@
 import { type Locator, type Page } from '@playwright/test';
 import { BasePage } from '../../infra/base-page';
 import { waitForElementToBeVisible } from '../../utils/wait-for-elements';
-
 export class Header extends BasePage {
-
     // LOCATORS
     private  globalButton:Locator | undefined
     private readonly tittlePages:Locator
-    private readonly justlanded ='//a[@href="/justlanded"]'
-    private readonly onSale='//a[@href="/on-sale"]'
-
-
+     categoriesData = {
+        justLanded:'//a[@href="/justlanded"]',
+        onSale:'//a[@href="/on-sale"]'
+    }
     constructor(page: Page) {
         super(page);
         this.tittlePages=this.page.locator('//h1[@class="h1-title_2hq_"]')
@@ -18,15 +16,37 @@ export class Header extends BasePage {
     }
     async clickOnHeaderButtonByText(li :string) {
         if(li == 'JUST LANDED'){
-            this.globalButton = this.page.locator(`//li[@class="item_1lit"]${this.justlanded}`);
+            this.globalButton = this.page.locator(`//li[@class="item_1lit"]${this.categoriesData.justLanded}`);
         }else{
-            this.globalButton = this.page.locator(`//li[@class="item_1lit"]${this.onSale}`);
+            this.globalButton = this.page.locator(`//li[@class="item_1lit"]${this.categoriesData.onSale}`);
         }
-        // await waitForElementToBeVisible(this.globalButton, 1000, 5)
+        await waitForElementToBeVisible(this.globalButton, 1000, 5)
         await this.globalButton.click();
     }
     async getTittle(){
         await waitForElementToBeVisible(this.tittlePages, 1000, 5)
         return await this.tittlePages.textContent()
     }
+    async hoverOnHeaderButtonByText(category: string | undefined){
+        let catregoryButton : Locator
+        catregoryButton = this.page.locator(`//a[@href='/${category}']`)
+        await waitForElementToBeVisible(catregoryButton,1000,5)
+        await catregoryButton.hover()
+        await this.page.waitForLoadState('load')
+    }
+    async getHoverAction(category :string|undefined){
+        let catregoryButton : Locator
+        catregoryButton = this.page.locator(`//li[contains(@class,"item_1lit") and a[@href='/${category}']]`)
+        await waitForElementToBeVisible(catregoryButton,1000,5)
+        await this.page.waitForLoadState("networkidle")
+        let className = await catregoryButton.getAttribute('class')
+        return  className?.includes('open_1RdF')
+    }
 }
+
+
+
+
+
+
+
--- tests/header-test.spec.ts
+++ tests/header-test.spec.ts
@@ -3,26 +3,30 @@ import configJson from '../configfiles/config.json'
 import { test, expect } from "@playwright/test";
 import { Page } from "playwright";
 import { Header } from "../logic/pages/header-page";
-
-
 test.describe('Test of clicked on header buttons', () => {
     let browserWrapper: BrowserWrapper;
     let page: Page;
-
     test.beforeEach(async () => {
         browserWrapper = new BrowserWrapper()
         page = await browserWrapper.getPage(configJson.url)
     });
-
     test.afterEach(async () => {
         await browserWrapper.closeBrowser();
     })
-
     const headerData = [
         { headerTittle: "JUST LANDED" },
-        { headerTittle: "ON SALE" }
+        { headerTittle: "ON SALE"},
     ];
-
+    const categoriesData = [
+        {category:"justlanded"},
+        {category:"on-sale"},
+        {category:"baby"},
+        {category:"brands"},
+        {category:"sports"},
+        {category:"beauty"},
+        {category:"home-lifestyle"},
+        {category:"premium"},
+    ]
     headerData.forEach((data, index) => {
         test(`Test Case ${index + 1} - click on - ${data.headerTittle} `, async () => {
             //ACT
@@ -32,4 +36,20 @@ test.describe('Test of clicked on header buttons', () => {
             expect(await header.getTittle()).toBe(data.headerTittle);
         });
     });
-});
+    categoriesData.forEach((data) => {
+        test(`Test Case  - hover on  - ${data.category} `, async () => {
+            //ACT
+            let header = new Header(page)
+            await header.hoverOnHeaderButtonByText(data.category);
+            //ASSERT
+           expect(await header.getHoverAction(data.category)).toBeTruthy()
+        });
+    });
+})
+
+
+
+
+
+
+
